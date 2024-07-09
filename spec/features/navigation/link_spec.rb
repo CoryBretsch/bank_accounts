7b323c86aed16b0c8298a74cecc459a32011f49d
Cory Bretsch <corybretsch@gmail.com>
2024-07-09 14:25:29 -0500

    user story 10

--- spec/features/navigation/link_spec.rb
+++ spec/features/navigation/link_spec.rb
@@ -35,5 +35,18 @@ RSpec.describe 'navigation links' do
       expect(current_path).to eq('/customers')
     end
   end
+
+  describe 'parent child index link' do 
+    it 'links to child index from parent show page' do  
+      customer = Customer.create!(name: "Leah Stromboli", birth_year: 1990, is_veteran: true)
+      checking = customer.accounts.create!(account_type: "Checking", balance: 24, has_veteran_status: false)
+      savings = customer.accounts.create!(account_type: "Savings", balance: 80, has_veteran_status: false)
+      visit "/customers/#{customer.id}"
+
+      click_link "/#{customer.name}'s Accounts"
+
+      expect(current_path).to eq("/customers/#{customer.id}/accounts")
+    end
   end
-end 
+  end 
+end
